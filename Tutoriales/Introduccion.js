b70ceeaa99c1c666eb2c4b3f31dea522e79f3939
Fredy Pallo <52755277+Freddyp666@users.noreply.github.com>
2024-09-27 12:03:14 -0500

    creando un index

---
+++ Tutoriales/Introduccion.js
@@ -0,0 +1,7 @@
+function changeColor(cambioColor){
+    const element=document.getElementById("textoColor")
+    element.style.color=cambioColor
+
+    const element2=document.getElementById("textoFoco")
+    element2.style.color=cambioColor
+}
