e90eec263624b413d7823115ec4533fd1ccf970b
Fredy Pallo <52755277+Freddyp666@users.noreply.github.com>
2024-09-30 15:50:58 +0000

    Script

--- Tutoriales/Introduccion.js
+++ Tutoriales/Introduccion.js
@@ -5,3 +5,7 @@ function changeColor(cambioColor){
     const element2=document.getElementById("textoFoco")
     element2.style.color=cambioColor
 }
+
+function cambioParrafo(){
+    document.getElementById("parrafo1").innerHTML="Esto es un parrafo que cambia cuando doy click el boton cambio de parrafo.";
+}
