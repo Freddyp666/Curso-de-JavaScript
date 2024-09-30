91ce9b78911445bb3af47889dddd3518726838ef
Fredy Pallo <52755277+Freddyp666@users.noreply.github.com>
2024-09-30 20:18:17 +0000

    Input suma y boton

--- Tutoriales/Introduccion.js
+++ Tutoriales/Introduccion.js
@@ -9,3 +9,12 @@ function changeColor(cambioColor){
 function cambioParrafo(){
     document.getElementById("parrafo1").innerHTML="Esto es un parrafo que cambia cuando doy click el boton cambio de parrafo.";
 }
+
+function funcionDeAlerta(){
+    window.alert("Este es un mensaje alerta")
+}
+
+function suma(x,y){
+    let x, y
+    return x+y
+}
