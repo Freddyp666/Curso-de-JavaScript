6577cd5969a9763d57515df91408495b1b0091fe
Fredy Pallo <52755277+Freddyp666@users.noreply.github.com>
2024-10-02 19:52:47 +0000

    comentarios

--- Tutoriales/Introduccion.js
+++ Tutoriales/Introduccion.js
@@ -1,3 +1,4 @@
+//Esta es la fucnion para cambiar el coler de texto
 function changeColor(cambioColor){
     const element=document.getElementById("textoColor")
     element.style.color=cambioColor
@@ -6,42 +7,51 @@ function changeColor(cambioColor){
     element2.style.color=cambioColor
 }
 
+/*Esta funcion cambia el texto del texto funcion inicial */
 function cambioParrafo(){
     document.getElementById("parrafo1").innerHTML="Esto es un parrafo que cambia cuando doy click el boton cambio de parrafo.";
 }
 
+//Esta fucion muestra una venta de mensaje de alerta
 function funcionDeAlerta(){
     window.alert("Este es un mensaje alerta")
 }
 
+//Esta funcion obtiene valores de ingresado en el input
 function obtenerValores() {
     let x = parseFloat(document.getElementById('n1').value);
     let y = parseFloat(document.getElementById('n2').value);
     return { x, y };
 }
 
+//Esta funcion imprime valores en la pantalla
 function mostrarResultado(resultado) {
     document.getElementById('resultado').innerText = `Resultado: ${resultado}`;
 }
 
+
+//Funcion de suma de dos valores
 function suma() {
     const { x, y } = obtenerValores();
     const resultado = x + y;
     mostrarResultado(resultado);
 }
 
+//Funcion de resta de dos valores
 function resta(){
     const {x,y}=obtenerValores();
     const resultado=x-y;
     mostrarResultado(resultado);
 }
 
+//Funcion de multiplicacion
 function multiplicacion(){
     const {x,y}=obtenerValores();
     const resultado=x*y;
     mostrarResultado(resultado);
 }
 
+//Funcion de division
 function division(){
     const {x,y}=obtenerValores();
     const resultado=x/y;
